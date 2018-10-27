a6c2fcf84c6d06d24d7b1a52cb01be07854f1419
Mark Clifton <mark.clifton@qrctech.com>
2018-10-27 13:55:23 -0400

    Camera Improvement

--- engine/camera/camera.cpp
+++ engine/camera/camera.cpp
@@ -5,38 +5,37 @@
 #include <math.h>
 
 void Camera3D::Update() {
-    projection = glm::perspective(field_of_view, aspect, static_cast<double>(m_clippingPlanes.x), static_cast<double>(m_clippingPlanes.y));
+    m_projection = glm::perspective(m_fov, m_aspect, static_cast<double>(m_clippingPlanes.x), static_cast<double>(m_clippingPlanes.y));
 
-    camera_direction = glm::normalize(camera_look_at - camera_position);
+    m_cameraPositionDelta = m_cameraDirection * m_cameraInputDelta.x;
+    m_cameraPositionDelta -= glm::cross(m_cameraDirection, m_cameraUp) * m_cameraInputDelta.y;
+    m_cameraPositionDelta += m_cameraUp * m_cameraInputDelta.z;
+
+    m_cameraDirection = glm::normalize(m_cameraLookAt - m_cameraPosition);
 
     // Calculate Quat for Pitch and Heading
-    glm::quat pitch_quat = glm::angleAxis(camera_pitch, glm::cross(camera_direction, camera_up));
-    glm::quat heading_quat = glm::angleAxis(camera_heading, camera_up);
+    glm::quat pitch_quat = glm::angleAxis(m_cameraPitch, glm::cross(m_cameraDirection, m_cameraUp));
+    glm::quat heading_quat = glm::angleAxis(m_cameraHeading, m_cameraUp);
 
     // Get New Camera Direction based on heading and pitch
-    camera_direction = glm::rotate(glm::normalize(glm::cross(pitch_quat, heading_quat)), camera_direction);
+    m_cameraDirection = glm::rotate(glm::normalize(glm::cross(pitch_quat, heading_quat)), m_cameraDirection);
 
-    camera_position += camera_position_delta;
-    camera_look_at = camera_position + camera_direction;
+    m_cameraPosition += m_cameraPositionDelta;
+    m_cameraLookAt = m_cameraPosition + m_cameraDirection;
 
-    // Stop all camera motion after updating
-    camera_heading *= .5f;
-    camera_pitch *= .5f;
-    camera_position_delta = camera_position_delta * .5f;
-
-    view = glm::lookAt(camera_position, camera_look_at, camera_up);
+    m_view = glm::lookAt(m_cameraPosition, m_cameraLookAt, m_cameraUp);
 }
 
 void Camera3D::SetFOV(double fov) {
-    field_of_view = fov;
+    m_fov = fov;
 }
 
 void Camera3D::SetViewport(int loc_x, int loc_y, int width, int height) {
-    viewport_x = loc_x;
-    viewport_y = loc_y;
-    window_width = width;
-    window_height = height;
-    aspect = width / double(height);
+    m_viewportX = loc_x;
+    m_viewportY = loc_y;
+    m_windowWidth = width;
+    m_windowHeight = height;
+    m_aspect = width / double(height);
 }
 
 void Camera3D::SetClipping(float near_clip_distance, float far_clip_distance) {
@@ -45,51 +44,121 @@ void Camera3D::SetClipping(float near_clip_distance, float far_clip_distance) {
 }
 
 void Camera3D::ChangePitch(float degrees) {
-    camera_pitch = fmod(camera_pitch + degrees,360.f);
+    m_cameraPitch = fmod(m_cameraPitch + degrees,360.f);
 }
 
 void Camera3D::ChangeHeading(float degrees) {
-    camera_heading = fmod(camera_heading + degrees,360.f);
+    m_cameraHeading = fmod(m_cameraHeading + degrees,360.f);
 }
 
 void Camera3D::GetMatricies(glm::mat4 &P, glm::mat4 &V) {
-    P = projection;
-    V = view;
+    P = m_projection;
+    V = m_view;
 }
 
-void Camera3D::process(int key, int scancode, int action, int mods)
+void Camera3D::process(int key, int /*scancode*/, int action, int /*mods*/)
 {
     switch (key)
     {
     case GLFW_KEY_W:
-        camera_position_delta += camera_direction * .05f;
+        if(action == GLFW_PRESS)
+        {
+            m_cameraInputDelta.x += m_movementSpeed;
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            m_cameraInputDelta.x -= m_movementSpeed;
+        }
         break;
     case GLFW_KEY_S:
-        camera_position_delta -= camera_direction * .05f;
+        if(action == GLFW_PRESS)
+        {
+            m_cameraInputDelta.x -= m_movementSpeed;
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            m_cameraInputDelta.x += m_movementSpeed;
+        }
         break;
     case GLFW_KEY_A:
-        camera_position_delta -= glm::cross(camera_direction, camera_up) * .05f;
+        if(action == GLFW_PRESS)
+        {
+            m_cameraInputDelta.y += m_movementSpeed;
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            m_cameraInputDelta.y -= m_movementSpeed;
+        }
         break;
     case GLFW_KEY_D:
-        camera_position_delta += glm::cross(camera_direction, camera_up) * .05f;
+        if(action == GLFW_PRESS)
+        {
+            m_cameraInputDelta.y -= m_movementSpeed;
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            m_cameraInputDelta.y += m_movementSpeed;
+        }
         break;
     case GLFW_KEY_E:
-        camera_position_delta += camera_up * .025f;
+        if(action == GLFW_PRESS)
+        {
+            m_cameraInputDelta.z += m_movementSpeed;
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            m_cameraInputDelta.z -= m_movementSpeed;
+        }
         break;
     case GLFW_KEY_Q:
-        camera_position_delta -= camera_up * .025f;
+        if(action == GLFW_PRESS)
+        {
+            m_cameraInputDelta.z -= m_movementSpeed;
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            m_cameraInputDelta.z += m_movementSpeed;
+        }
         break;
     case GLFW_KEY_UP:
-        ChangePitch(.025f);
+        if(action == GLFW_PRESS)
+        {
+            ChangePitch(m_rotationSpeed);
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            ChangePitch(-m_rotationSpeed);
+        }
         break;
     case GLFW_KEY_DOWN:
-        ChangePitch(-.025f);
+        if(action == GLFW_PRESS)
+        {
+            ChangePitch(-m_rotationSpeed);
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            ChangePitch(m_rotationSpeed);
+        }
         break;
     case GLFW_KEY_LEFT:
-        ChangeHeading(.025f);
+        if(action == GLFW_PRESS)
+        {
+            ChangeHeading(m_rotationSpeed);
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            ChangeHeading(-m_rotationSpeed);
+        }
         break;
     case GLFW_KEY_RIGHT:
-        ChangeHeading(-.025f);
+        if(action == GLFW_PRESS)
+        {
+            ChangeHeading(-m_rotationSpeed);
+        }
+        else if(action == GLFW_RELEASE)
+        {
+            ChangeHeading(m_rotationSpeed);
+        }
         break;
     }
 }
--- engine/camera/camera.h
+++ engine/camera/camera.h
@@ -29,33 +29,31 @@ public:
 
     void GetMatricies(glm::mat4 &P, glm::mat4 &V);
 
-    //TODO: Improve these
     void process(int key, int scancode, int action, int mods) override;
 
-    inline const glm::vec3& GetPosition() { return camera_position; }
+    inline const glm::vec3& GetPosition() { return m_cameraPosition; }
 private:
-    int viewport_x;
-    int viewport_y;
-
-    int window_width;
-    int window_height;
-
-    double aspect { 4/3.};
-    double field_of_view {45};
+    int m_viewportX;
+    int m_viewportY;
+    int m_windowWidth;
+    int m_windowHeight;
+    double m_aspect { 4/3.};
+    double m_fov {45};
     glm::vec2 m_clippingPlanes {.01f, 1000};
 
-    float camera_heading {0.f};
-    float camera_pitch {0.f};
+    glm::mat4 m_projection {1.f};
+    glm::mat4 m_view {1.f};
 
-    glm::vec3 camera_position {0,0,1};
-    glm::vec3 camera_position_delta {0,0,0};
-    glm::vec3 camera_look_at {0,0,0};
-    glm::vec3 camera_direction {0,0,0};
+    float m_cameraHeading {0.f};
+    float m_cameraPitch {0.f};
 
-    glm::vec3 camera_up {0,1,0};
-    glm::vec3 mouse_position {0,0,0};
+    glm::vec3 m_cameraDirection {0,0,0};
+    glm::vec3 m_cameraInputDelta {0.f};
+    glm::vec3 m_cameraLookAt {0,0,0};
+    glm::vec3 m_cameraPosition {0,0,1};
+    glm::vec3 m_cameraPositionDelta {0,0,0};
+    glm::vec3 m_cameraUp {0,1,0};
 
-    glm::mat4 projection {1.f};
-    glm::mat4 view {1.f};
-
+    float m_movementSpeed {.065f};
+    float m_rotationSpeed {.035f};
 };
